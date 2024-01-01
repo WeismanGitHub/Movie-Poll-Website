84bf533fdcf7235865e66b03bdecffeace6315f6
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-01 10:35:17 -0800

    created router

---
+++ Client/src/router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+// import About from './pages/about.tsx';
+// import Auth from './pages/auth.tsx';
+// import Home from './pages/home.tsx';
+
+const router = createBrowserRouter([
+    // { path: '/About', element: <About /> },
+    // { path: '/Auth', element: <Auth /> },
+    // { path: '/', element: <Home /> },
+]);
+
+export default router;
