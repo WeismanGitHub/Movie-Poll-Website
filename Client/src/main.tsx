ad0fbdb7551fc9f333f75197f5c66e020e1e7138
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-01 10:35:02 -0800

    use react router dom

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client';
+import router from './router';
+import React from 'react';
+import '../theme.css';
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+          <RouterProvider router={router} />
+    </React.StrictMode>
+);
