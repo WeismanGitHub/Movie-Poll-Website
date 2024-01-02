0af9d1afcc3e208c0118a4cc49521b6a692ca996
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-02 00:19:01 -0800

    compiles

--- Server/Controllers/Polls.cs
+++ Server/Controllers/Polls.cs
@@ -5,14 +5,10 @@ namespace Letterboxd_Poll_Website.Server.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class PollsController : ControllerBase {
-    private readonly ILogger<pollsController> _logger;
-
-    public pollsController(ILogger<PollsController> logger) {
-        _logger = logger;
-    }
 
     [HttpPost(Name = "Create Poll")]
-    public IEnumerable<WeatherForecast> Post() {
+    public string Post() {
         Console.WriteLine("test");
+		return "test";
     }
 }
