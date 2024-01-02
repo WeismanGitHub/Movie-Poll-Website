4532da88732061d4b8cb5c9ae85dc747d8007a13
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-02 00:20:44 -0800

    removed extra css stuff for now

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import router from './router';
 import React from 'react';
-import '../theme.css';
-import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
