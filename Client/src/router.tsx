bcf671f4248812551f68447bd69a5553da49c3b7
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-02 10:30:28 -0800

    have homepage route

--- Client/src/router.tsx
+++ Client/src/router.tsx
@@ -1,13 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-// import About from './pages/about.tsx';
-// import Auth from './pages/auth.tsx';
-// import Home from './pages/home.tsx';
+import Home from './pages/home.tsx';
 
 const router = createBrowserRouter([
-    // { path: '/About', element: <About /> },
-    // { path: '/Auth', element: <Auth /> },
-    // { path: '/', element: <Home /> },
+    { path: '/', element: <Home /> },
 ]);
 
 export default router;
