62996266fe69afb9beb763362efcce29b2cf141b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-03 15:37:57 -0800

    updated schemas

--- Server/Database/dbContext.cs
+++ Server/Database/dbContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
+using System.ComponentModel.DataAnnotations;
+
 public class LbPollContext : DbContext {
     public DbSet<Poll> Polls { get; set; }
 
@@ -8,18 +10,26 @@ public class LbPollContext : DbContext {
     public LbPollContext() {
         var folder = Environment.SpecialFolder.LocalApplicationData;
         var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "lettreboxd-poll.db");
+        DbPath = Path.Join(path, "lettreboxd-poll.db");
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
 }
 
 public class Vote {
-    public string userId { get; set; }
-    public string choiceId { get; set; }
+	[Key, Required]
+    public required string UserId { get; set; }
+	[Required]
+    public string ChoiceId { get; set; }
 }
 
 public class Poll {
-    public string Id { get; set; }
-    public List<Vote> Votes { get; } = new();
+	[Key, Required]
+	public Guid Id { get; set; } = Guid.NewGuid();
+	[Required]
+	public required string Question {  get; set; }
+	[Required]
+	public List<Vote> Votes { get; set; } = new();
+	[Required]
+	public string? ServerId { get; set; }
 }
