9b11d63541bfddae4a0554857b98867f2f8b8bc0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-03 20:20:29 -0800

    created CreatePoll endpoint

--- Server/Controllers/Polls.cs
+++ Server/Controllers/Polls.cs
@@ -1,14 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using Database;
 
 namespace Letterboxd_Poll_Website.Server.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/polls")]
 public class PollsController : ControllerBase {
+	public class CreatePollDTO {
+		public string Question { get; set; }
+		public DateTime? Expiration { get; set; }
+	}
 
-    [HttpPost(Name = "Create Poll")]
-    public string Post() {
-        Console.WriteLine("test");
-		return "test";
+	[HttpPost(Name = "CreatePoll")]
+	[ProducesResponseType(StatusCodes.Status201Created)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	public IActionResult CreatePoll(CreatePollDTO input) {
+		using var db = new LbPollContext();
+
+		var poll = new Poll {
+			Question = input.Question,
+			Expiration = input.Expiration,
+		};
+
+		db.Add(poll);
+		db.SaveChanges();
+
+		return Created(string.Empty, poll.Id);
     }
 }
