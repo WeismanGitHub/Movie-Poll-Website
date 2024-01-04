78000978d130a11f9b2f8abde78c05702ed00834
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-03 19:32:46 -0800

    split poll schema into separate file

---
+++ Server/Database/Poll.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database;
+
+public class Vote {
+	[Key, Required]
+	public required string UserId { get; set; }
+	[Required]
+	public string ChoiceId { get; set; }
+}
+
+public class Poll {
+	[Key, Required]
+	public Guid Id { get; set; } = Guid.NewGuid();
+	[Required]
+	public required string Question { get; set; }
+	[Required]
+	public List<Vote> Votes { get; set; } = new();
+	[Required]
+	public string? ServerId { get; set; }
+}
--- Server/Database/dbContext.cs
+++ Server/Database/dbContext.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 
-using System.ComponentModel.DataAnnotations;
-
+namespace Database;
 public class LbPollContext : DbContext {
     public DbSet<Poll> Polls { get; set; }
 
@@ -14,22 +13,4 @@ public class LbPollContext : DbContext {
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
-}
-
-public class Vote {
-	[Key, Required]
-    public required string UserId { get; set; }
-	[Required]
-    public string ChoiceId { get; set; }
-}
-
-public class Poll {
-	[Key, Required]
-	public Guid Id { get; set; } = Guid.NewGuid();
-	[Required]
-	public required string Question {  get; set; }
-	[Required]
-	public List<Vote> Votes { get; set; } = new();
-	[Required]
-	public string? ServerId { get; set; }
 }
