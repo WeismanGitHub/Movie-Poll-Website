48ebb7d2e1e7ac7c1ec7f00a0fb1a6d3752196c0
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-06 22:30:55 -0800

    set up all the routes

--- Client/src/router.tsx
+++ Client/src/router.tsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import CreatePoll from './pages/create-poll.tsx';
 import Home from './pages/home.tsx';
+import Auth from './pages/auth.tsx';
+import Poll from './pages/poll.tsx';
 
 const router = createBrowserRouter([
+    { path: '/polls/:pollId', element: <Poll /> },
+    { path: '/create/', element: <CreatePoll /> },
+    { path: '/auth', element: <Auth /> },
     { path: '/', element: <Home /> },
 ]);
 
