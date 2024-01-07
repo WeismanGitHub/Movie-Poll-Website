d9606192726bd04a3155c069ed7078882aa98e36
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-06 20:58:05 -0800

    created settings

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,13 @@
+using API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var settings = builder.Configuration.Get<Settings>()!;
+builder.Services.AddSingleton(settings);
 
 var app = builder.Build();
 
---
+++ Server/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace API;
+
+public sealed class Settings {
+	public DiscordSettings Discord { get; set; }
+	public string TmdbKey { get; set; }
+	public class DiscordSettings {
+		public string RedirectUri { get; set; }
+		public string Secret { get; set; }
+		public string Id { get; set; }
+	}
+}
