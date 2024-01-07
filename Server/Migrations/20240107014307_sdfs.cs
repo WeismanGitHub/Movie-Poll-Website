cd59c7ea317b112351e1ef20155f32224b597be3
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-06 17:45:26 -0800

    migrated

---
+++ Server/Migrations/20240107014307_sdfs.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class sdfs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ChoiceId",
+                table: "Vote",
+                newName: "ItemId");
+
+            migrationBuilder.RenameColumn(
+                name: "MediaIds",
+                table: "Polls",
+                newName: "ItemIds");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ItemId",
+                table: "Vote",
+                newName: "ChoiceId");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemIds",
+                table: "Polls",
+                newName: "MediaIds");
+        }
+    }
+}
