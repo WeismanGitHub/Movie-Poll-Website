e0857780bf75180384e4710d4534f0a93d95dedb
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-06 18:02:53 -0800

    created new controller

---
+++ Server/Controllers/Self.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Poll_Website.Server.Controllers;
+
+[ApiController]
+[Route("api/self")]
+public class SelfController : ControllerBase {
+	[HttpGet(Name = "GetGuilds")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	public async Task<IActionResult> GetGuilds() {
+		var authCode = Request.Headers.Authorization;
+		Console.WriteLine(authCode);
+
+		return Ok();
+	}
+}
