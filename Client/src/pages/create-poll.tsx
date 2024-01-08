b414d9d449cdab681424aba447f4647d9834641d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-08 06:11:23 -0800

    got datepicker working

--- Client/src/pages/create-poll.tsx
+++ Client/src/pages/create-poll.tsx
@@ -1,26 +1,50 @@
-import { ToastContainer, Toast, Modal, Button, Row, Form, Col } from 'react-bootstrap';
-import { useState } from 'react';
-import { Formik } from 'formik';
+import { ToastContainer, Toast, Button, Row, Form, Col } from 'react-bootstrap';
+import DateTimePicker from 'react-datetime-picker';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Field, Formik } from 'formik';
 import * as yup from 'yup';
 
 export default function CreatePoll() {
+    const [expirationError, setExpirationError] = useState<string | null>(null);
+    const [expiration, setExpiration] = useState<Date | null>(null);
+    const [error, setError] = useState<object | null>(null);
+    const [showError, setShowError] = useState(false);
+    const [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        const pollId = searchParams.get('pollId');
+        const state = searchParams.get('state');
+        const code = searchParams.get('code');
+
+        console.log(pollId, state, code);
+        error;
+        setError;
+    }, []);
+
+    useEffect(() => {
+        if (expiration && expiration.valueOf() <= Date.now()) {
+            setExpirationError('Must expire in the future.');
+        } else {
+            setExpirationError(null);
+        }
+    }, [expiration]);
+
     const schema = yup.object().shape({
-        conversationID: yup
+        question: yup
             .string()
-            .required('Convo ID is a required field.')
+            .required('Question is a required field.')
             .min(1, 'Must be at least 1 characters.')
-            .max(25, 'Cannot be more than 25 characters.'),
+            .max(500, 'Cannot be more than 500 characters.'),
     });
 
-    const [toastError, setToastError] = useState<object | null>(null);
-    const [showError, setShowError] = useState(false);
-    const [showModal, setShowModal] = useState(false);
-
-    async function createPoll() {}
+    async function createPoll(values: { question: string }) {
+        console.log(values);
+    }
 
     return (
-        <>
-            {/* <ToastContainer position="top-end">
+        <div className="container vh-100 text-center align-items-center justify-content-center flex">
+            <ToastContainer position="top-end">
                 <Toast
                     onClose={() => setShowError(false)}
                     show={showError}
@@ -30,63 +54,74 @@ export default function CreatePoll() {
                 >
                     <Toast.Header>
                         <strong className="me-auto">
-                            {toastError?.message || 'Unable to read error name.'}
+                            {/* {error?.message || 'Unable to read error name.'} */}
                         </strong>
                     </Toast.Header>
                     <Toast.Body>
-                        {toastError?.errors &&
-                            Object.values(toastError?.errors).map((err) => {
+                        {/* {error?.errors &&
+                            Object.values(error?.errors).map((err) => {
                                 return <div key={err}>{err}</div>;
-                            })}
+                            })} */}
                     </Toast.Body>
                 </Toast>
-            </ToastContainer> */}
+            </ToastContainer>
 
-            <Modal show={showModal}>
-                <Modal.Dialog>
-                    <Modal.Header closeButton onClick={() => setShowModal(false)}></Modal.Header>
-                    <Modal.Body>
-                        <div className="w-100">
-                            <Formik
-                                validationSchema={schema}
-                                validateOnChange
-                                onSubmit={createPoll}
-                                initialValues={{ conversationID: '' }}
-                            >
-                                {({ handleSubmit, handleChange, values, errors }) => (
-                                    <Form noValidate onSubmit={handleSubmit}>
-                                        <Row className="mb-3">
-                                            <Form.Group as={Col} controlId="conversationID">
-                                                <Form.Label>Question</Form.Label>
-                                                <Form.Control
-                                                    type="text"
-                                                    name="conversationID"
-                                                    value={values.conversationID}
-                                                    onChange={handleChange}
-                                                    isInvalid={!!errors.conversationID}
-                                                />
-                                                <Form.Control.Feedback type="invalid">
-                                                    {errors.conversationID}
-                                                </Form.Control.Feedback>
-                                            </Form.Group>
-                                        </Row>
-                                        <Button type="submit">Join</Button>
-                                    </Form>
-                                )}
-                            </Formik>
-                        </div>
-                    </Modal.Body>
-                </Modal.Dialog>
-            </Modal>
-
-            <div className="container justify-content-center">
-                <div className="row">
-                    <div className="btn btn-primary btn-lg" onClick={() => setShowModal(true)}>
-                        Create Poll
-                    </div>
-                </div>
-                <div className="row"></div>
+            <div className="row bg-white rounded shadow p-2">
+                <Formik
+                    validationSchema={schema}
+                    validateOnChange
+                    onSubmit={createPoll}
+                    initialValues={{ question: '', expirationToggle: false }}
+                >
+                    {({ handleSubmit, handleChange, values, errors, setFieldValue }) => (
+                        <Form noValidate onSubmit={handleSubmit}>
+                            <Row className="mb-2">
+                                <Form.Group as={Col}>
+                                    <Form.Label>Question</Form.Label>
+                                    <Form.Control
+                                        as="textarea"
+                                        type="textarea"
+                                        rows={4}
+                                        name="question"
+                                        value={values.question}
+                                        onChange={handleChange}
+                                        isInvalid={!!errors.question}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.question}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                            </Row>
+                            <Row className="mb-2">
+                                <Form.Group as={Col}>
+                                    <Form.Label className="me-1">Expiration</Form.Label>
+                                    <Field
+                                        type="checkbox"
+                                        name="expirationToggle"
+                                        onChange={() => {
+                                            setFieldValue('expirationToggle', !values.expirationToggle);
+                                            setExpiration(null);
+                                            setExpirationError(null);
+                                        }}
+                                    />
+                                    <Form.Control hidden={true} isInvalid={Boolean(expirationError)} />
+                                    <br />
+                                    {values.expirationToggle && (
+                                        <DateTimePicker
+                                            onChange={(value) => setExpiration(value)}
+                                            value={expiration}
+                                        />
+                                    )}
+                                    <Form.Control.Feedback type="invalid">
+                                        {expirationError}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                            </Row>
+                            <Button type="submit">Create</Button>
+                        </Form>
+                    )}
+                </Formik>
             </div>
-        </>
+        </div>
     );
 }
