792c4b32e8a44ebeb53d88114fe62ebb35a3dc5b
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-08 06:10:26 -0800

    added datetime picker css

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import router from './router';
 import React from 'react';
 
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <RouterProvider router={router} />
