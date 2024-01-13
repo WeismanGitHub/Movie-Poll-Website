d11ec465c718158cb7fbf2cd15086f83f4274548
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-12 21:58:14 -0800

    migrated

--- Server/Migrations/20240113043750_ChangedItems.Designer.cs
+++ Server/Migrations/20240113043750_ChangedItems.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace server.Migrations
 {
     [DbContext(typeof(LbPollContext))]
-    partial class LbPollContextModelSnapshot : ModelSnapshot
+    [Migration("20240113043750_ChangedItems")]
+    partial class ChangedItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
