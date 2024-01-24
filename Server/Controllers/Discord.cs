72a53b3c5ffe53c74b6d370c5beaa1c7a4074aa3
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-01-24 12:19:52 -0800

    split misc

---
+++ Server/Controllers/Discord.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Net;
+using server;
+using API;
+
+namespace Movie_Poll_Website.Server.Controllers;
+
+[ApiController]
+[Route("api/discord")]
+public class DiscordController : ControllerBase {
+	private readonly Settings _settings;
+
+	public DiscordController(Settings settings) {
+		_settings = settings;
+	}
+
+	[HttpGet("token", Name = "GetAccessToken")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	public async Task<IActionResult> GetAccessToken([FromQuery(Name = "code")] string authCode) {
+		var discord = new DiscordOauth2(_settings);
+		var accessToken = await discord.GetAccessToken(authCode);
+
+		return Ok(accessToken);
+	}
+
+	[HttpGet("guilds", Name = "GetGuilds")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	public async Task<IActionResult> GetGuilds([FromQuery(Name = "token")] string accessToken) {
+		var discord = new DiscordOauth2(_settings);
+		var guilds = await discord.GetGuilds(accessToken);
+
+		return Ok(guilds);
+	}
+}
--- Server/Controllers/TMDb.cs
+++ Server/Controllers/TMDb.cs
@@ -8,30 +8,11 @@ namespace Movie_Poll_Website.Server.Controllers;
 
 [ApiController]
 [Route("api")]
-public class MiscController : ControllerBase {
+public class TMDbController : ControllerBase {
 	private readonly Settings _settings;
 
-	public MiscController(Settings settings) {
+	public TMDbController(Settings settings) {
 		_settings = settings;
-	}
-
-	public class OAuthData {
-		public List<DiscordOauth2.Guild> Guilds { get; set; }
-		public string AccessToken { get; set; }
-	}
-
-	[HttpGet("oauth", Name = "GetOAuthData")]
-	[ProducesResponseType(StatusCodes.Status200OK)]
-	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-	public async Task<IActionResult> GetOAuthData([FromQuery(Name = "code")] string authCode) {
-		var discord = new DiscordOauth2(_settings);
-		var accessToken = await discord.GetAccessToken(authCode);
-		var guilds = await discord.GetGuilds(accessToken);
-
-		return Ok(new OAuthData {
-			Guilds = guilds,
-			AccessToken = accessToken
-		});
 	}
 
 	[HttpGet("search", Name = "SearchMovie")]
