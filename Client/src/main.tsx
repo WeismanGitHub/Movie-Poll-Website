2b48823761b8ac1f41103949f3f64eeb684bacf5
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-02-03 23:17:54 -0800

    removed css stuff for time selection package i deleted earlier

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,12 +1,9 @@
 import { RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import router from './router';
 import React from 'react';
 
-import 'react-datetime-picker/dist/DateTimePicker.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-calendar/dist/Calendar.css';
-import 'react-clock/dist/Clock.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
