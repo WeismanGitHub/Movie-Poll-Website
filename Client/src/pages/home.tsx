8b1509e560d54ac6d88d585c6ae1329c0f04e61e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-02-03 23:45:14 -0800

    finished

--- Client/src/pages/home.tsx
+++ Client/src/pages/home.tsx
@@ -1,13 +1,16 @@
 export default function Home() {
     return (
-        <div className="container justify-content-center">
-            <div className="row">
-                <a href="/create" className="btn btn-primary btn-lg" role="button">
-                    Create Poll
-                </a>
-                <div />
+        <div className="container justify-content-center align-items-center vh-100 vw-100 text-center fs-4 p-2 d-flex">
+            <div>
+            <h1>Easily create polls and engage in a collective movie-selection process.</h1>
+            Integrate seamlessly with Discord to restrict polls to server members.
+            <br/>
+            <div>Built with <strong>C#</strong>, <strong>Typescript</strong>, <strong>ASP.NET</strong>, <strong>SQL</strong>, and <strong>React</strong>.</div>
+            <br/>
+            <a href="/create" className="btn btn-primary btn-lg" role="button">
+                Create Poll
+            </a>
             </div>
-            <div className="row"></div>
         </div>
     );
 }
