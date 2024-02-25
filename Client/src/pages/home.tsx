c0353b8bc55d55b724a316c289bd5ed57a7d58b2
weisman <weisman123@outlook.com>
2024-02-25 03:35:21 -0800

    updated description

--- Client/src/pages/home.tsx
+++ Client/src/pages/home.tsx
@@ -4,17 +4,11 @@ export default function Home() {
     return (
         <>
             <Navbar />
-            <div className="container justify-content-center align-items-center text-center fs-4 p-2 d-flex">
-                <div>
-                    <h1>Easily create polls and engage in a collective movie-selection process.</h1>
-                    Integrate seamlessly with Discord to restrict polls to server members.
-                    <br />
-                    <div>
-                        Built with <strong>C#</strong>, <strong>Typescript</strong>, <strong>ASP.NET</strong>,{' '}
-                        <strong>SQL</strong>, and <strong>React</strong>.
-                    </div>
-                    <br />
-                </div>
+            <div
+                className="container d-flex justify-content-center align-items-center text-center fs-4 p-2 flex-column"
+                style={{ height: '75vh' }}
+            >
+                <h1>Easily create movie polls and seamlessly integrate them with Discord, allowing only server members to participate.</h1>
             </div>
         </>
     );
