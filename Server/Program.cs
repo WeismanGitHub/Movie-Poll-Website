af551b5772d23b51d901dae659850031fb18076d
weisman <weisman123@outlook.com>
2024-03-01 18:45:06 -0800

    added some more middleware

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,18 +5,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHsts(options => {
+	options.IncludeSubDomains = true;
+});
 
 var settings = builder.Configuration.Get<Settings>()!;
 builder.Services.AddSingleton(settings);
 
 var app = builder.Build();
 
+app.UseHsts();
+app.UseResponseCaching();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseDefaultFiles();
-app.UseResponseCaching();
 app.UseStaticFiles();
 app.MapFallbackToFile("/index.html");
 app.MapControllers();
