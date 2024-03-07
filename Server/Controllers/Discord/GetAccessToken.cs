76291e5a9600fa9aa23550b55fb69ebccf00dc50
weisman <weisman123@outlook.com>
2024-03-07 06:54:46 -0800

    Split GetAccessToken into separate file

---
+++ Server/Controllers/Discord/GetAccessToken.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("API/Discord")]
+public class GetAccessTokenController : ControllerBase
+{
+    [HttpGet("token", Name = "GetAccessToken")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAccessToken(
+        [FromQuery(Name = "code"), Required, MaxLength(50)] string authCode,
+        Settings settings
+    )
+    {
+        var discord = new DiscordOauth2(settings);
+        var accessToken = await discord.GetAccessToken(authCode);
+
+        return Ok(accessToken);
+    }
+}
