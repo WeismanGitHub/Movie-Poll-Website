28b3e9e6e75d95e4d99f61909ae11f40642ea019
weisman <weisman123@outlook.com>
2024-03-07 06:51:41 -0800

    Split TMDb endpoints

--- Server/Controllers/TMDb/Search.cs
+++ Server/Controllers/TMDb/Search.cs
@@ -1,41 +1,27 @@
-﻿using System.Net;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
 using System.Net.Http.Headers;
 
 namespace Server.Controllers;
 
 [ApiController]
-[Route("api/tmdb")]
-public class TMDbController : ControllerBase
+[Route("API/TMDb")]
+public class SearchController : ControllerBase
 {
-    private readonly Settings _settings;
-
-    public TMDbController(Settings settings)
-    {
-        _settings = settings;
-    }
-
     [HttpGet("search", Name = "SearchMovie")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> SearchMovie(
-        [FromQuery(Name = "query")] string query,
-        [FromQuery(Name = "page")] string page
+        [FromQuery(Name = "query"), MaxLength(500), MinLength(1), Required] string query,
+        [FromQuery(Name = "page"), MaxLength(500), MinLength(1), Required] string page,
+        Settings settings
     )
     {
-        if (query.Length > 500 || query.Length < 1)
-        {
-            return BadRequest("Query must be between 1 and 500 characters.");
-        }
-        else if (page.Length > 500 || page.Length < 1)
-        {
-            return BadRequest("Page must be between 1 and 500.");
-        }
-
         var client = new HttpClient();
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
             "Bearer",
-            _settings.TmdbKey
+            settings.TmdbKey
         );
 
         HttpResponseMessage res = await client.GetAsync(
