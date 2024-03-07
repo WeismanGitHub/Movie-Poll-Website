78fc1285b494ae30fe0f59097f773ded9e73f931
weisman <weisman123@outlook.com>
2024-03-07 06:56:32 -0800

    Split GetGuilds into separate file

---
+++ Server/Controllers/Discord/GetGuilds.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("API/Discord")]
+public class GetGuildsController : ControllerBase
+{
+    [HttpGet("Guilds", Name = "GetGuilds")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetGuilds(
+        [FromQuery(Name = "token"), Required, MaxLength(50)] string accessToken,
+        Settings settings
+    )
+    {
+        var discord = new DiscordOauth2(settings);
+        List<DiscordOauth2.Guild> guilds = await discord.GetGuilds(accessToken);
+
+        return Ok(guilds);
+    }
+}
