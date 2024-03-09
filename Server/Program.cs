a3cb9ff700d631412c160f9316f137f572dc735b
weisman <weisman123@outlook.com>
2024-03-09 13:49:55 -0800

    Reorder some lines

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,13 +1,12 @@
 using Server;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+Settings settings = builder.Configuration.Get<Settings>()!;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHsts(options => options.IncludeSubDomains = true);
-
-Settings settings = builder.Configuration.Get<Settings>()!;
 builder.Services.AddSingleton(new DiscordOauth2(settings));
 builder.Services.AddSingleton(settings);
 builder.Services.AddScoped<HttpClient>();
